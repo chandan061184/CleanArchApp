262e9e2128e76ca60c3349b9fc11d02f2fe2138c
Chandan Kumar <chandan061184@outlook.com>
2020-04-25 12:32:34 +0530

    Added UI for Courses Injected presentation layer to get courses

---
+++ CleanArch/CleanArch.MVC/Pages/Course.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CleanArch.MVC.Pages
+{
+    public class CourseModel : PageModel
+    {
+        private ICourseService _courseService;
+        public CourseViewModel Course { get; set; }
+        public CourseModel(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public void OnGet()
+        {
+            Course = _courseService.GetAllCourses(); 
+        }
+    }
+}
